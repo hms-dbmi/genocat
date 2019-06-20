153a77218c2e3986433efd0c4c38931c6ef2d091
Justine <mac@Justines-MacBook-Pro.local>
2019-06-20 18:54:34 -0400

    checkbox search updates

--- scripts/safe one.js
+++ scripts/safe one.js
@@ -62,3 +62,14 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
