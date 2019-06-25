9112f80a6d2614a536b301bc8aa1c3acf021a9a2
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-06-25 14:56:31 -0400

    citedby

--- data_splice.py
+++ data_splice.py
@@ -5,6 +5,6 @@ for line in f1:
         curr_tool = line[9:]
         f2.write(line)
     if line[2:9] == "citedby":
-        f2.write("  cited by: "+line[12:].replace(',', ''))
+        f2.write("  citedby: "+line[12:].replace(',', ''))
 f1.close()
 f2.close()
