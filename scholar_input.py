174434f7c478f18bf924a937008c96b49e1c82b5
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-06-25 11:52:41 -0400

    python file reads from tools pages (frontmatter) and prints citations!

--- scholar_input.py
+++ scholar_input.py
@@ -1,16 +1,10 @@
 import scholarly
+import frontmatter
 
-title = 'CGView - Circular Genome Viewer'
-paper_title = 'CiVi: circular genome visualization with unique features to analyze sequence elements'
+post = frontmatter.load('_tools/CGView.markdown')
+f = open('_data/scholar_data.txt', 'w')
+paper_title = post['paper_title']
+title = post['title']
 search_query = scholarly.search_pubs_query(paper_title)
-
-f2 = open('_data/scholar_data.txt', 'w')
-f2.write("- title: '"+title+"'\n"+"  ")
-f2.write(str(next(search_query))+"\n\n")
-f2.close();
-
-#f2 = open('_data/scholar_data.txt', 'a')
-#for post in site.tools:
-    #paper_title = post.paper_title
-    #f2.write("- paper title: '"+paper_title+"'\n"+"  ")
-#f2.close()
+f.write("- title: "+title+"\n  "+str(next(search_query))+"\n\n")
+f.close()
