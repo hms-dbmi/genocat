1873c52bb06427829192e5086241a257db4e8ae8
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-16 12:17:15 -0400

    implementing checkbox filter with all tools

--- scripts/filter.js
+++ scripts/filter.js
@@ -20,22 +20,60 @@ function filterFunction() {
   }
 }
 
-filterMultSelection([]);
+// filterMultSelection([]);
+//
+// function filterMultSelection(c) {
+//   var x, i, j;
+//   x = document.getElementsByClassName("filterDiv");
+//   for (i = 0; i < x.length; i++) {
+//     j=0;
+//     addTool(x[i], "show");
+//     while (j<c.length) {
+//       if (x[i].className.indexOf(c[j]) == -1 && c.length>0) {
+//         removeTool(x[i], "show");
+//       }
+//       j++;
+//     }
+//   }
+// }
 
-function filterMultSelection(c) {
-  var x, i, j;
-  x = document.getElementsByClassName("filterDiv");
-  for (i = 0; i < x.length; i++) {
+checkbox_filter([]);
+
+function checkbox_filter(tags) {
+  var tools, i, j, or;
+  tools = document.getElementsByClassName("filterDiv");
+  for (i = 0; i < tools.length; i++) {
+    var tagList = tools[i].className.split(" ");
+    //console.log("tool is "+tagList[1]);
     j=0;
-    addTool(x[i], "show");
-    while (j<c.length) {
-      if (x[i].className.indexOf(c[j]) == -1 && c.length>0) {
-        removeTool(x[i], "show");
+    addTool(tools[i], "show");
+    while (j<tags.length) {
+      //console.log("curr category tags are "+tags[j]);
+      //console.log("length of tags["+j+"] is "+tags[j].length);
+      or = false;
+      if (tags[j].length == 0) {
+        or = true;
+      }
+      k=0;
+      while (k < tags[j].length) {
+        //console.log("curr tag is "+tags[j][k]);
+        if (tools[i].className.indexOf(tags[j][k]) != -1 || tags[j].length == 0) {
+          //if tool contains at least one of the tags
+          //console.log("found match: tool is "+tagList[1]+" tag is"+tags[j][k]);
+          or = true;
+        }
+        k++;
+      }
+      if (or == false) {
+        //console.log("did not find any matches: tool is "+tagList[1]+" category "+tags[j]);
+        removeTool(tools[i], "show");
+        break;
       }
       j++;
     }
   }
 }
+
 
 // Show filtered elements
 function addTool(element, name) {
