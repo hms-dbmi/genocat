58c3e18d954d2757ef431635e3204843dd46021b
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-22 14:59:44 -0400

    removing imported frontmatter piece

--- from_frontmatter.py
+++ from_frontmatter.py
@@ -1,8 +1,6 @@
-# import frontmatter
 import os
 
 def get_attribute(attr, lines):
-    # lines = f_tool.readlines()
     length = len(attr)
     j=0
     while j<len(lines):
@@ -11,17 +9,10 @@ def get_attribute(attr, lines):
         j+=1
     return 0
 
-
 f1 = open('scholar_data.txt', 'w')
 f2 = open('_data/scholar_data.yml', 'w')
 for file in os.listdir('_tools'):
     filepath = os.path.join('_tools', file)
-
-    # post = frontmatter.load(filepath)
-    # title = post['title']
-    # doi = post['doi']
-    # citation_count = post['citation_count']
-
     f_tool = open(filepath, 'r')
     lines = f_tool.readlines()
     title = get_attribute('title', lines)
@@ -34,8 +25,5 @@ for file in os.listdir('_tools'):
     f2.write("  "+doi)
     f1.write("  "+citation_count)
     f2.write("  "+citation_count)
-    # f.write("- title: "+title+"\n");
-    # f.write("  doi: "+doi+"\n");
-    # f.write("  citedby: "+str(citation_count)+"\n");
 f1.close()
 f2.close()
