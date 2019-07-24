3c201969f1471a264c89154996a65c27639fbbbe
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-24 16:35:51 -0400

    fixing "=" syntax

--- cron.sh
+++ cron.sh
@@ -3,10 +3,10 @@
 echo "travis event type is $TRAVIS_EVENT_TYPE"
 cron_var="cron"
 push_var="push"
-if [ "$TRAVIS_EVENT_TYPE" == "$cron_var" ]; then
+if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
   echo "hello"
   python data_splice.py
   python to_frontmatter.py
-elif [ "$TRAVIS_EVENT_TYPE" == "$push_var" ]; then
+elif [ "$TRAVIS_EVENT_TYPE" = "$push_var" ]; then
   echo "travis event type is not cron"
 fi
