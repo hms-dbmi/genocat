5fc4161ef3421d58173c3e6b992b01016bc46340
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-24 16:23:49 -0400

    add spaces in if clause

--- cron.sh
+++ cron.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 echo "travis event type is $TRAVIS_EVENT_TYPE"
-if [$TRAVIS_EVENT_TYPE == 'cron']
-then
+cron_var = "cron"
+push_var = "push"
+if [ "$TRAVIS_EVENT_TYPE" == "$cron_var" ]; then
   echo "hello"
   python data_splice.py
   python to_frontmatter.py
-elif [$TRAVIS_EVENT_TYPE == 'push']
-then
+elif [ "$TRAVIS_EVENT_TYPE" == "$push_var" ]; then
   echo "travis event type is not cron"
 fi
