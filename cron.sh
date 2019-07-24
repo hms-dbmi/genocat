61bb22af00719f096845ceca518d9d5cbf6fc747
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-24 16:10:51 -0400

    triggering build for event type testing

--- cron.sh
+++ cron.sh
@@ -1,10 +1,10 @@
 echo "travis event type is $TRAVIS_EVENT_TYPE"
-if ["$TRAVIS_EVENT_TYPE" == "cron"]
+if [$TRAVIS_EVENT_TYPE == 'cron']
 then
   echo "hello"
   python data_splice.py
   python to_frontmatter.py
-elif ["$TRAVIS_EVENT_TYPE" != "cron"]
+elif [$TRAVIS_EVENT_TYPE != 'cron']
 then
   echo "travis event type is not cron"
 fi
