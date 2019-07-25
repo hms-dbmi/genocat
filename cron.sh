edd14a05ac3fe72819ab167c2f60f16b43728583
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-25 14:41:04 -0400

    removing elif push

--- cron.sh
+++ cron.sh
@@ -6,5 +6,4 @@ if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
   # python scholar_input.py
   python data_splice.py
   python to_frontmatter.py
-elif [ "$TRAVIS_EVENT_TYPE" = "$push_var" ]; then
 fi
