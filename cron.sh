31e57165c4e147de4b16c9cc80d432356c6a65bc
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-07-25 14:10:28 -0400

    relative/absolute position of filterDiv elements

--- cron.sh
+++ cron.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "travis event type is $TRAVIS_EVENT_TYPE"
 cron_var="cron"
 push_var="push"
 if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
@@ -8,5 +7,4 @@ if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
   python data_splice.py
   python to_frontmatter.py
 elif [ "$TRAVIS_EVENT_TYPE" = "$push_var" ]; then
-  echo "travis event type is not cron"
 fi
