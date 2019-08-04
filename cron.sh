9faf19567f92d92a4501c0533cffdcbaa5aaa8eb
Claudia Meyer <claudiameyer@Claudias-MacBook-Pro.local>
2019-08-04 14:13:12 -0400

    cron.sh add python folder

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 
 cron_var="cron"
 if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
-  # python scholar_input.py
-  # python data_splice.py
-  python to_frontmatter.py
+  # python python/scholar_input.py
+  # python python/data_splice.py
+  python python/to_frontmatter.py
 fi
