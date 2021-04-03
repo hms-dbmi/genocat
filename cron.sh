0cf265993f5ee509251902ec8ae9c3ed2be6ecb1
Christian Stolte <christian.stolte@gmail.com>
2021-04-03 15:26:52 -0400

    add missing files

---
+++ cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cron_var="cron"
+if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
+  # python python/scholar_input.py
+  # python python/data_splice.py
+  python python/to_frontmatter.py
+fi
