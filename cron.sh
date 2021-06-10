c58e0a2f8f56c6fc9d6b749695cc377c2e51477c
justineshih <jys43@cornell.edu>
2021-06-09 22:47:58 -0400

    changed cron.sh

--- cron.sh
+++ cron.sh
@@ -4,5 +4,5 @@ cron_var="cron"
 if [ "$TRAVIS_EVENT_TYPE" = "$cron_var" ]; then
   # python python/scholar_input.py
   # python python/data_splice.py
-  python python/to_frontmatter.py
+  # python python/to_frontmatter.py
 fi
